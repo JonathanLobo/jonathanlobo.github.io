8803ffe766733a4651ecb1b7e25e1052bcedd34c
Jonathan Lobo <jpl68@pitt.edu>
2016-10-10 19:52:34 -0400

    Made major progress on site

--- js/init.js
+++ js/init.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
-    $('.slider').slider({full_width: true});
+  $('.slider').slider({full_width: true});
+  $('.card-text').hide();
+});
+
+$("#card1-div").mouseover(function(){
+    $("#card1").show();
+});
+
+$("#card1-div").mouseout(function(){
+    $("#card1").hide();
+});
+
+$("#card2-div").mouseover(function(){
+    $("#card2").show();
+});
+
+$("#card2-div").mouseout(function(){
+    $("#card2").hide();
+});
+
+$("#card3-div").mouseover(function(){
+    $("#card3").show();
+});
+
+$("#card3-div").mouseout(function(){
+    $("#card3").hide();
 });
