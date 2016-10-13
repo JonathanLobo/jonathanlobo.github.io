004f64d6a28489d0ea2d7526af25e977653beaf4
Jonathan Lobo <jpl68@pitt.edu>
2016-10-13 00:23:05 -0400

    Formatted code to follow style guide, renamed js file

--- js/mysite.js
+++ js/mysite.js
@@ -12,45 +12,45 @@ $(document).ready(function () {
     adaptiveHeight: true
   });
   $('.scrollspy').scrollSpy();
-  $("#errorText1").hide();
-  $("#errorText2").hide();
-  $("#errorText3").hide();
-  $("#successText").hide();
+  $('#errorText1').hide();
+  $('#errorText2').hide();
+  $('#errorText3').hide();
+  $('#successText').hide();
 });
 
 $(document).on('click', '#submitButton', function(e) {
   e.preventDefault();
-  $("#errorText1").hide();
-  $("#errorText2").hide();
-  $("#errorText3").hide();
-  $("#successText").hide();
+  $('#errorText1').hide();
+  $('#errorText2').hide();
+  $('#errorText3').hide();
+  $('#successText').hide();
 
   if (validate()) {
-    $("#successText").show();
+    $('#successText').show();
   }
 });
 
 function validate() {
-  var name = document.getElementById("nameBox").value;
-  var email = document.getElementById("email").value;
-  var comments = document.getElementById("comments").value;
+  var name = document.getElementById('nameBox').value;
+  var email = document.getElementById('email').value;
+  var comments = document.getElementById('comments').value;
 
   var emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
   var ret = true;
 
   if (name === '') {
-    $("#errorText1").show();
+    $('#errorText1').show();
     ret = false;
   }
 
   if (!emailRegex.test(email)) {
-    $("#errorText2").show();
+    $('#errorText2').show();
     ret = false;
   }
 
   if (comments === '') {
-    $("#errorText3").show();
+    $('#errorText3').show();
     ret = false;
   }
 
